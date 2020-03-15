1847136ea3abb2f640c6b86b77863c67d6455de0
waiferkolar <72coder@gmail.com>
2020-03-15 13:02:43 +0630

    Flask Api Done

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, flash,session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
+from flask_bcrypt import Bcrypt
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///m.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+app.secret_key = "asdfasdf2323"
+
+UPLOAD_FOLDER = "./static/imgs"
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 class Category(db.Model):
@@ -18,6 +27,7 @@ class Category(db.Model):
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    cat_id = db.Column(db.Integer)
     title = db.Column(db.String(50), nullable=False)
     content = db.Column(db.Text, nullable=False)
     image = db.Column(db.String(200), nullable=False)
@@ -31,7 +41,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow())
 
     def __repr__(self):
@@ -46,6 +56,48 @@ def home():
     cat = Category.query.get(1)
     print(cat)
     return render_template("home.html", context=context)
+
+
+@app.route("/posts")
+def posts():
+    context = {
+        "title": "Category Home Page",
+        "posts": Post.query.all(),
+    }
+    return render_template("posts.html", context=context)
+
+
+@app.route("/post/create", methods=["POST", "GET"])
+def postCreate():
+    context = {
+        "title": "Category Home Page",
+        "cats": Category.query.all()
+    }
+    if request.method == "POST":
+        filename = ""
+        file = request.files['image']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect("/posts")
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        cat_id = request.form["cat_id"]
+        title = request.form["title"]
+        content = request.form["content"]
+        post = Post(cat_id=cat_id, title=title,
+                    content=content, image=filename)
+        try:
+            db.session.add(post)
+            db.session.commit()
+            return redirect("/posts")
+        except:
+            return render_template("post_create.html", context=context)
+    else:
+        return render_template("post_create.html", context=context)
 
 
 @app.route("/cats")
@@ -102,6 +154,63 @@ def catDelete(id):
     db.session.delete(cat)
     db.session.commit()
     return redirect("/cats")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    context = {
+        "title": "User Register Page"
+    }
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        password = bcrypt.generate_password_hash(password)
+        user = User(name=name, email=email, password=password)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return render_template("register.html", context=context)
+    else:
+        return render_template("register.html", context=context)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    context = {
+        "title": "User Login Page"
+    }
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        user = User.query.filter_by(email=email).first()
+        if user : 
+            if bcrypt.check_password_hash(user.password, password):
+                flash("Welcome Back")
+                session["username"] =user.name
+                session["email"] =user.email
+                return redirect("/")
+            else:
+                print("Password Fail")
+                return redirect("/login")
+        else : 
+            print("Email Error!")
+            return redirect('/login')
+    else:
+        return render_template("login.html", context=context)
+# Helpers Methods
+
+@app.route("/logout")
+def logout():
+    session["username"] = ""
+    session["email"] = ""
+    return redirect("/login")
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 if __name__ == "__main__":
